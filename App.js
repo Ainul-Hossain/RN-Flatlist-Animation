4073526a8950301c568df42b039e560dada5dfe6
ainul-hossain <ainulsakibs@gmail.com>
2023-12-17 01:24:06 +0600

    Flatlist Animation done!

--- App.js
+++ App.js
@@ -1,11 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import * as Animatable from 'react-native-animatable';
 
-export default function App() {
+const App = () => {
+
+  const [data, setData] = useState([
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+  ])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <FlatList
+        style={{ marginVertical: 100 }}
+        data={data}
+        renderItem={({ item, index }) => {
+          return (
+            <Animatable.View
+              animation={'fadeInUp'}
+              duration={1000}
+              delay={index * 300}
+              style={{ height: 70, width: '100%' }}
+            >
+              <TouchableOpacity
+                style={{
+                  padding: 10,
+                  borderWidth: .2,
+                  justifyContent: 'center',
+                  alignItems: 'center'
+                }}
+              >
+                <Text style={{ fontSize: 30 }}>{`Item ${index + 1}`}</Text>
+              </TouchableOpacity>
+            </Animatable.View>
+          )
+        }}
+      />
     </View>
   );
 }
@@ -14,7 +55,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
+
+export default App;
